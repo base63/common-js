fa4ee30b93d4553c2481612eff69268bdaf66215
Horia Coman <horia141@gmail.com>
2017-10-11 16:57:34 +0300

    Add tests for LocaleFromLanguageMarshaller

--- src/language-test.ts
+++ src/language-test.ts
@@ -1,9 +1,49 @@
 import { expect } from 'chai'
 import 'mocha'
 
-import { LanguageMarshaller } from './language'
+import { LanguageMarshaller, LanguageFromLocaleMarshaller } from './language'
 
 const languages = require('iso-639-1');
+const countryCodes = require('iso-3166-1-alpha-2');
+
+
+describe('LanguageFromLocaleMarshaller', () => {
+    const NonLanguageCodes = [
+        '',
+        'hello',
+        'CZ',
+        'fr',
+        'fr+EN'
+    ];
+
+    describe('extract', () => {
+        for (let langCode of languages.getAllCodes()) {
+            for (let countryCode of countryCodes.getCodes().slice(0, 3)) {
+                it (`should parse "${langCode}-${countryCode}"`, () => {
+                    const languageFromLocaleMarshaller = new LanguageFromLocaleMarshaller();
+                    expect(languageFromLocaleMarshaller.extract(`${langCode}-${countryCode}`)).to.equal(langCode);
+                    expect(languageFromLocaleMarshaller.extract(`${langCode}_${countryCode}`)).to.equal(langCode);
+                });
+            }
+        }
+
+        for (let nonLangCode of NonLanguageCodes) {
+            it (`should throw for non-lang-code "${nonLangCode}`, () => {
+                const languageFromLocaleMarshaller = new LanguageFromLocaleMarshaller();
+                expect(() => languageFromLocaleMarshaller.extract(nonLangCode)).to.throw('Expected a valid locale');
+            });
+        }
+    });
+
+    describe('pack', () => {
+        for (let langCode of languages.getAllCodes()) {
+            it (`should produce the same input for "${langCode}`, () => {
+                const languageFromLocaleMarshaller = new LanguageFromLocaleMarshaller();
+                expect(languageFromLocaleMarshaller.pack(langCode)).to.equal(langCode);
+            });
+        }
+    });
+})
 
 
 describe('LanguageMarshaller', () => {
@@ -34,7 +74,7 @@ describe('LanguageMarshaller', () => {
             it (`should produce the same input for "${langCode}`, () => {
                 const languageMarshaller = new LanguageMarshaller();
                 expect(languageMarshaller.pack(langCode)).to.equal(langCode);
-            })
+            });
         }
     });
 
@@ -48,7 +88,7 @@ describe('LanguageMarshaller', () => {
                 const packed = languageMarshaller.pack(extracted);
 
                 expect(packed).to.equal(raw);
-            })
+            });
         }
     });
 })
--- src/language.ts
+++ src/language.ts
@@ -4,6 +4,17 @@ const localeCode = require('locale-code')
 const languages = require('iso-639-1')
 
 
+export class LanguageMarshaller extends StringMarshaller {
+    filter(language: string): string {
+        if (!languages.validate(language)) {
+            throw new ExtractError('Expected a valid language');
+        }
+
+        return language;
+    }
+}
+
+
 export class LanguageFromLocaleMarshaller extends StringMarshaller {
     filter(locale: string): string {
         const cleanedLocale = locale.replace('_', '-');
@@ -13,16 +24,5 @@ export class LanguageFromLocaleMarshaller extends StringMarshaller {
         }
 
         return localeCode.getLanguageCode(cleanedLocale);
-    }
-}
-
-
-export class LanguageMarshaller extends StringMarshaller {
-    filter(language: string): string {
-        if (!languages.validate(language)) {
-            throw new ExtractError('Expected a valid language');
-        }
-
-        return language;
     }
 }
