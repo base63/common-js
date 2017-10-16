0a5b8c56022d13672fa971a6e8d2dea96066404c
Horia Coman <horia141@gmail.com>
2017-10-16 14:35:29 +0300

    Add comments for the currency module

--- src/currency.ts
+++ src/currency.ts
@@ -1,19 +1,36 @@
+/** @module Defines {@link Currency} and associated helpers. */
+
+/** Imports. Also so tsdoc works correctly.  */
+
 import { BaseStringMarshaller, ExtractError } from 'raynor'
 
-
+/**
+ * A currency.
+ */
 export class Currency {
     private readonly _name: string;
 
-    constructor(name: string) {
-        this._name = name;
+    /**
+     * Constructs a currency with a given [ISO4217]{@link https://en.wikipedia.org/wiki/ISO_4217} code.
+     * @params code - The ISO4217 currency code.
+     */
+    constructor(code: string) {
+        this._name = code;
     }
 
+    /**
+     * Obtain the string representation of a currency instance.
+     * @returns The string representation of a currency instance.
+     */
     toString(): string {
         return this._name;
     }
 }
 
 
+/**
+ * The set of allowed currencies. Used by {@link CurrencyMarshaller} as a whitelist.
+ */
 export const StandardCurrencies: any = {
     EUR: new Currency('EUR'),
     USD: new Currency('USD'),
@@ -21,6 +38,9 @@ export const StandardCurrencies: any = {
 };
 
 
+/**
+ * A Marshaller for {@link Currency} objects.
+ */
 export class CurrencyMarshaller extends BaseStringMarshaller<Currency> {
     build(a: string): Currency {
         if (!StandardCurrencies.hasOwnProperty(a)) {
