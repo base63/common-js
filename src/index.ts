876f82f4d17711b38e8b7d00cbbe0f69002cbf01
Horia Coman <horia141@gmail.com>
2017-11-10 13:07:01 +0200

    Be explicit about what we export from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-export * from './context'
-export * from './currency'
-export * from './env'
-export * from './language'
-export * from './slugify'
+export {
+    Context,
+    parseContext,
+    isClient,
+    isServer
+} from './context'
+
+export {
+    Currency,
+    CurrencyMarshaller
+} from './currency'
+
+export {
+    Env,
+    parseEnv,
+    envToString,
+    isLocal,
+    isOnServer
+} from './env'
+
+export {
+    LanguageMarshaller,
+    LanguageFromLocaleMarshaller
+} from './language'
+
+export {
+    slugify
+} from './slugify'
